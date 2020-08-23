39601e1faa4ebbca08ce84386c23e00debe19617
Maxim Filimonov <tpaktopsp@gmail.com>
2020-08-23 07:15:45 +0000

    add basic example of monday trigger which is working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,47 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as express from "express";
+import * as jwt from "jsonwebtoken";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+interface MondayToken {
+  userId: string;
+  accountId: string;
+  backToUrl: string;
+}
+function isMondayToken(object: any): object is MondayToken {
+  return "backToUrl" in object;
+}
+
+app.get("/", (req, res) => {
+  const token = req.query.token;
+  if (typeof token === "string") {
+    const verifiedToken = jwt.verify(
+      token,
+      functions.config().monday_acuity.signature
+    );
+    if (isMondayToken(verifiedToken)) {
+      const { userId, accountId, backToUrl } = verifiedToken;
+      console.log({ userId, accountId, backToUrl });
+    }
+  }
+  res.redirect("/auth.html");
+});
+
+app.get("/timestamp", (req, res) => {
+  res.send(`${new Date()}`);
+});
+
+app.post("/subscribe", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.post("/unsubscribe", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get("/acuity-oauth2-callback", (req, res) => {
+  res.send("Hello");
+});
+
+export const server = functions.https.onRequest(app);
