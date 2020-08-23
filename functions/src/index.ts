770738b4038ecfaf69fb784732824b69168b2629
Maxim Filimonov <tpaktopsp@gmail.com>
2020-08-23 08:16:47 +0000

    add unit tests

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 import * as jwt from "jsonwebtoken";
+import * as admin from "firebase-admin";
+admin.initializeApp();
 
 const app = express();
 
@@ -13,19 +15,32 @@ function isMondayToken(object: any): object is MondayToken {
   return "backToUrl" in object;
 }
 
-app.get("/", (req, res) => {
-  const token = req.query.token;
+export function saveMondayToken(token: MondayToken) {
+  return admin.firestore().doc("/backToUrl").create(token);
+}
+export function verifyAndExtractMondayToken(token: any) {
   if (typeof token === "string") {
     const verifiedToken = jwt.verify(
       token,
       functions.config().monday_acuity.signature
     );
     if (isMondayToken(verifiedToken)) {
-      const { userId, accountId, backToUrl } = verifiedToken;
-      console.log({ userId, accountId, backToUrl });
+      return Promise.resolve(verifiedToken);
+    } else {
+      return Promise.reject(
+        `not a valid monday token ${JSON.stringify(verifiedToken)}`
+      );
     }
+  } else {
+    return Promise.reject("token is not supplied or not a string");
   }
-  res.redirect("/auth.html");
+}
+app.get("/", (req, res) => {
+  const token = req.query.token;
+
+  return verifyMondayToken(token).then(() => {
+    res.redirect("/auth.html");
+  });
 });
 
 app.get("/timestamp", (req, res) => {
